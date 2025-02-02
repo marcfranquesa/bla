29c72b5035cc2e0f5bdb574c0fe9ad0185630974
Marc Franquesa Monés <git@marcfranquesa.com>
2025-02-02 10:49:15 -0500

    add option to remove volumes in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,28 @@
 #!/usr/bin/env bash
 
+#!/usr/bin/env bash
+
+RESET_VOLUMES=false
+
+for arg in "$@"; do
+    case $arg in
+        --reset-volumes)
+        RESET_VOLUMES=true
+        shift
+        ;;
+    *)
+        ;;
+    esac
+done
+
 ln -sf ./build/package/Dockerfile .
 ln -sf ./deployments/compose.yaml .
 
-docker-compose down -v
+if [ "$RESET_VOLUMES" = true ]; then
+    docker-compose down -v
+else
+    docker-compose down
+fi
+
 docker-compose up --build
+
