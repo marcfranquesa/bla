056304d50170fe3ab3af31c30f157c7e737924ae
Marc Franquesa Monés <git@marcfranquesa.com>
2025-02-11 15:34:27 -0500

    remove volumes flag from deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,28 +1,7 @@
 #!/usr/bin/env bash
 
-#!/usr/bin/env bash
-
-RESET_VOLUMES=false
-
-for arg in "$@"; do
-    case $arg in
-        --reset-volumes)
-        RESET_VOLUMES=true
-        shift
-        ;;
-    *)
-        ;;
-    esac
-done
-
 ln -sf ./build/package/Dockerfile .
-ln -sf ./deployments/compose.yaml .
+ln -sf ./deployments/docker-compose.mysql.yaml docker-compose.yaml
 
-if [ "$RESET_VOLUMES" = true ]; then
-    docker-compose down -v
-else
-    docker-compose down
-fi
-
+docker-compose down
 docker-compose up --build
-
