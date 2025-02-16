ad2969dd04f6a2d4a3be9e877f160a179f03c8d0
Marc Franquesa Monés <git@marcfranquesa.com>
2025-02-16 13:04:19 -0500

    refactor: remove package/ subdir in build/

--- scripts/deploy.mysql.moderation.sh
+++ scripts/deploy.mysql.moderation.sh
@@ -4,8 +4,8 @@ VOLUME_NAME="bla_data"
 
 docker volume inspect $VOLUME_NAME > /dev/null 2>&1 || docker volume create $VOLUME_NAME
 
-ln -sf ./build/package/Dockerfile .
-ln -sf ./build/package/Dockerfile.moderation-tui .
+ln -sf ./build/Dockerfile .
+ln -sf ./build/Dockerfile.moderation-tui .
 ln -sf ./deployments/docker-compose.mysql.moderation.yaml docker-compose.yaml
 
 docker-compose up --build -d
