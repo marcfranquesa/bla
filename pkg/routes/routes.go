2be496fcb99f718dcde48134999c318ec400134a
Marc Franquesa Monés <git@marcfranquesa.com>
2025-02-25 13:51:40 -0500

    refactor: remove request logs

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -1,9 +1,7 @@
 package routes
 
 import (
-	"log"
 	"net/http"
-	"strings"
 
 	"github.com/marcfranquesa/bla/pkg/config"
 	"github.com/marcfranquesa/bla/pkg/handlers"
@@ -11,14 +9,6 @@ import (
 
 func SetupRoutes(cfg config.ServerConfig) {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		ip := r.Header.Get("X-Forwarded-For")
-		if ip != "" {
-			ip = strings.Split(ip, ",")[0]
-		} else {
-			ip = r.RemoteAddr
-		}
-		log.Printf("Client IP: %s, Method: %s, URL: %s, Proto: %s\n", ip, r.Method, r.URL, r.Proto)
-
 		if r.Method == http.MethodGet {
 			handlers.ServeStaticFiles(w, r)
 		} else if r.Method == http.MethodPost {
